c706cdbd5518d3fc4aa47060b26ff95cc5a4e279
Danila Loginov <danila@loginov.rocks>
2020-11-20 23:19:05 +0300

    fix: Redeploy services

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  products: 'https://jmw5ojxm2a.execute-api.eu-central-1.amazonaws.com/dev/products',
+  products: 'https://d7667crufa.execute-api.eu-central-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://78s203i22m.execute-api.eu-central-1.amazonaws.com/dev/import',
 };
