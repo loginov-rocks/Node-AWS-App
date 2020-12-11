652df23e5ee7434ed3ef4b52afe997457fb9fd48
Danila Loginov <danila@loginov.rocks>
2020-12-11 20:37:27 +0300

    Proxy implemented

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -1,20 +1,26 @@
-import { All, Controller, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { All, Controller, Req, Res } from '@nestjs/common';
+import { Method } from 'axios';
+import { Request, Response } from 'express';
 
 import { DiscoveryService } from './discovery.service';
+import { ProxyService } from './proxy.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly discoveryService: DiscoveryService) {
+  constructor(
+      private readonly discoveryService: DiscoveryService,
+      private readonly proxyService: ProxyService,
+  ) {
   }
 
   @All()
-  proxy(@Req() req: Request) {
+  async proxy<T>(@Req() req: Request, @Res() res: Response): Promise<void> {
+    const method = req.method as Method;
     const url = this.discoveryService.getEndpointUrl(req.path);
+    const body = req.body;
 
-    return [
-      url,
-      req.method,
-    ];
+    const { data, status } = await this.proxyService.request(method, url, body);
+
+    res.status(status).send(data);
   }
 }
--- bff-service/src/app.module.ts
+++ bff-service/src/app.module.ts
@@ -1,18 +1,21 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { DiscoveryService } from './discovery.service';
+import { ProxyService } from './proxy.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    HttpModule,
   ],
   controllers: [
     AppController,
   ],
   providers: [
     DiscoveryService,
+    ProxyService,
   ],
 })
 export class AppModule {
