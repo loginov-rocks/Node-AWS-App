3cb1ea6cc5ef7c86e753838e65755c28c6a33d3d
Danila Loginov <danila@loginov.rocks>
2020-12-13 18:48:59 +0300

    Configure BFF and Cart API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@ const API_PATHS = {
   products: 'https://d7667crufa.execute-api.eu-central-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://78s203i22m.execute-api.eu-central-1.amazonaws.com/dev/import',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://loginov-rocks-cart-api-develop.eu-central-1.elasticbeanstalk.com/api',
+  bff: 'http://loginov-rocks-bff-api-dev.eu-central-1.elasticbeanstalk.com',
+  cart: 'http://loginov-rocks-cart-api-dev.eu-central-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
